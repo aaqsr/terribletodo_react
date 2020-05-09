82dceb9d3dd5387ea0bef5ace469eaebf77a7e84
Awab Qureshi <awab.qureshi@gmail.com>
2020-05-09 12:22:38 +0500

    Clicking text toggles the checkbox

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 let id = 2; // 0, 1 reserved for example
+// possible bug? eventually this number might overflow? since it doesn't get reset unless the page is refreshed. but that wont happen right
+// right?
 
 const Todo = props => { // How a todo item looks like:
     console.log(props);
     return (
         <div>
             <li>
-                 <input type="checkbox" checked={props.todo.checked} onClick={props.onToggle}/> {/* todo is checked or not based on that props.todo.checked property */}
-                 <span>{props.todo.text} </span>
+                 <input type="checkbox" checked={props.todo.checked} onClick={props.onToggle}/>     {/* todo is checked or not based on that props.todo.checked property */}
+                 <span onClick={props.onToggle}>    {/* Let's user click on the text and still have it toggle */}
+                    {props.todo.text}
+                 </span>
                  <button onClick={props.onDelete} >Delete</button>
             </li>
         </div>
@@ -36,7 +40,7 @@ class App extends React.Component {
     }
 
     addTodo() { // called when button is clicked to add todo item
-        // TODO: replace this with a textbox not just a prompt
+        // TODO: replace this with a text box not just a prompt
         const inputText = prompt("TODO text?");
         this.setState({
             todos: [
@@ -52,13 +56,13 @@ class App extends React.Component {
 
     removeTodo(id) {
         this.setState({
-            todos: this.state.todos.filter((todo) => (todo.id !== id) )
+            todos: this.state.todos.filter((todo) => (todo.id !== id))
         });
     }
 
     toggleTodo(id) {
         this.setState({
-            todos: this.state.todos.map( (todo) => {
+            todos: this.state.todos.map((todo) => {
                 if (id !== todo.id) return todo;
                 return {
                     id: todo.id,
@@ -83,7 +87,7 @@ class App extends React.Component {
                 <button onClick={() => this.addTodo()}>Add TODO</button>
                 <ul>
                     {this.state.todos.map(todo => ( <Todo todo={todo} onDelete={() => this.removeTodo(todo.id)} onToggle={() => this.toggleTodo(todo.id)}/> ) )}
-                    {/* Takes each element in the array and passes it one by one with the name of "todo" as the prop of the Todo component. Also passes unique functions to delete that todo or toggle that todo with that todo's id. */}
+                    {/* Takes each element in the array and passes it one by one with the name of "todo" as the prop of the Todo component. Also passes unique functions to delete that todo or toggle that todo with that todos id. */}
                 </ul>
             </div>
         )
