2550c8405011574b4b90729b3e020d46d78d335a
Awab Qureshi <awab.qureshi@gmail.com>
2020-05-09 11:05:13 +0500

    Added delete by passing unique function to each todo with its id

--- src/index.js
+++ src/index.js
@@ -2,44 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Todo = props => {
+const Todo = props => { // How a todo item looks like:
     console.log(props);
     return (
         <div>
             <li>
                  <input type="checkbox"/>
                  <span>{props.todo.text} </span>
-                 <button>Delete</button>
+                 <button onClick={props.onDelete} >Delete</button>
             </li>
         </div>
     )
 }
 
-let id = 2;     // 0, 1 reserved for example
+let id = 2; // 0, 1 reserved for example
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            todos: [
-                {id: 0, text:"something"},
-                {id: 1, text:"another example"},
+        this.state = { // declare state, comes with a few examples
+            todos: [{
+                    id: 0,
+                    text: "something"
+                },
+                {
+                    id: 1,
+                    text: "another example"
+                },
             ],
         }
     }
 
-    addTodo() {
+    addTodo() { // called when button is clicked to add todo item
         // TODO: replace this with a textbox not just a prompt
         const inputText = prompt("TODO text?");
-        this.setState({todos: [...this.state.todos, {id: id++, text:inputText}]});
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: id++,
+                    text: inputText
+                }
+            ]
+        });
+    }
+
+    removeTodo(id) {
+        this.setState({
+            todos: this.state.todos.filter((todo) => (todo.id !== id) )
+        })
     }
 
     render() {
-        return (
+        return ( // renders the app
             <div>
             <button onClick={() => this.addTodo()}>Add TODO</button>
             <ul>
-                {this.state.todos.map(todo => (<Todo todo={todo}/>))}   {/* Maps over the entire array, one element at a time*/}
+                {this.state.todos.map(todo => ( <Todo todo={todo} onDelete={() => this.removeTodo(todo.id)} /> ) )}
+                {/* Takes each element in the array and passes it one by one with the name of "todo" as the prop of the Todo component. Also passes a unique function to delete that todo with that todo's id. */}
             </ul>
             </div>
         )
