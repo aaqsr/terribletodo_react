f6f86ecc24bef29f2252a2473e2e7a468c2e91f1
Awab Qureshi <awab.qureshi@gmail.com>
2020-05-09 10:19:54 +0500

    Added button to add todo

--- src/index.js
+++ src/index.js
@@ -2,40 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
- // <input type="checkbox" />
- //                <span>{}</span>
- //                <span><button>delete</button></span>
-
 const Todo = props => {
     console.log(props);
     return (
-    <div>
-        <li>
-            <input type="checkbox"/>
-            <span>{props.todo} </span>
-            <button>Delete</button>
-        </li>
-    </div>
-)
+        <div>
+            <li>
+                 <input type="checkbox"/>
+                 <span>{props.todo} </span>
+                 <button>Delete</button>
+            </li>
+        </div>
+    )
 }
 
 class App extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
             todos: ["something", "another example", "etc"],
         }
     }
-    render () {
+
+    addTodo() {
+        // TODO: replace this with a textbox not just a prompt
+        const text = prompt("TODO text?");
+        this.setState({todos: [...this.state.todos, text]});
+    }
+
+    render() {
         return (
+            <div>
+            <button onClick={() => this.addTodo()} />
             <ul>
                 {this.state.todos.map(todo => (<Todo todo={todo}/>))}
             </ul>
+            </div>
         )
-   }
+    }
 }
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <div>
+        <App />
+    </div>,
+    document.getElementById('root')
 );
