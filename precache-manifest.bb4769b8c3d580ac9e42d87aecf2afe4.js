9dbd306035b52b58eb24847237e81360f535ecc9
Awab Qureshi <awab.qureshi@gmail.com>
2020-05-10 10:05:19 +0500

    Updates

---
+++ precache-manifest.bb4769b8c3d580ac9e42d87aecf2afe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d57d3a1788528fb6e3018331dfa81b",
+    "url": "/terribletodo_react/index.html"
+  },
+  {
+    "revision": "b3dd784af5e5bcd82c64",
+    "url": "/terribletodo_react/static/css/main.8e045474.chunk.css"
+  },
+  {
+    "revision": "8be80eaa3946e152b303",
+    "url": "/terribletodo_react/static/js/2.1c6ff295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/terribletodo_react/static/js/2.1c6ff295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3dd784af5e5bcd82c64",
+    "url": "/terribletodo_react/static/js/main.f39d40d3.chunk.js"
+  },
+  {
+    "revision": "ca1bdd1ccb1940f724b1",
+    "url": "/terribletodo_react/static/js/runtime-main.56e252eb.js"
+  }
+]);
